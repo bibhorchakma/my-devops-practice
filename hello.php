5a4acf6a4dcec67a0747b926509602322d68b63b
Soikat Chakma <soikat.chakma@gmail.com>
2018-04-26 10:17:16 +1000

    Revert "after unstashing file"
    This reverts commit cd24703e44e61a0f17765eab4bce44f1eb793701.

--- hello.php
+++ hello.php
@@ -5,6 +5,5 @@
  <body>
  <?php echo '<p>Hello World!!!</p>'; ?> 
  <?php echo '<p>Working with this file, while there is some changes are stashed</p>'; ?> 
- <?php echo '<p>Hello Soikat. This line will be stashed Now</p>'; ?> 
  </body>
 </html>
